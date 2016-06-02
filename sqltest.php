321cdc1b4d09b9592058ff017a5c4bf38538dc28
Ryan Rynda <rrynda@wiktel.com>
2016-06-02 16:00:15 -0500

    First Commit
    This is the first commit of these files.

---
+++ sqltest.php
@@ -0,0 +1,18 @@
+<?php
+$mysqli = new mysqli('DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME');
+
+$query = "SELECT * FROM books";
+#$query = "SELECT title FROM books";
+
+#make a title bar denoting what is displayed
+echo "<h2>Book_ID\tTitle\tYear_Published\tShelf_ID</h2>\n";
+
+if($result = $mysqli->query($query))
+{
+    while($row = $result->fetch_assoc())
+    {
+        #print_r($row);
+        #echo "<h2>Title: </h2>" . $row["title"] . "<br>\n";
+        printf ("<p>%s\t%s\t%s\t%s</p>\n", $row["id"], $row["title"], $row["year_published"], $row["shelf_id"]);
+    }
+}
