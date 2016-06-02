85a8b45cea91ed83188c9d520c17e1835f57e84e
Ryan Rynda <rrynda@wiktel.com>
2016-06-02 16:05:16 -0500

    Comment Added
    A brief comment was added explaining the purpose of the while loop

--- sqltest.php
+++ sqltest.php
@@ -9,6 +9,7 @@ echo "<h2>Book_ID\tTitle\tYear_Published\tShelf_ID</h2>\n";
 
 if($result = $mysqli->query($query))
 {
+    #while loop to run through and print all the data in the books table
     while($row = $result->fetch_assoc())
     {
         #print_r($row);
