283666b0b29d4f81c87c52ac6af528bc0fdbfc0e
Ryan Rynda <rrynda@wiktel.com>
2016-06-06 15:26:28 -0500

    Links
    Means of linking to other web pages for the purpose of sorting the database have been added.

--- sqltest.php
+++ sqltest.php
@@ -19,12 +19,33 @@ $query = "SELECT * FROM books";
 <body>
 <h2>Welcome to the Library</h2>
 
+<h4><a href='http://rrynda.wiktel.com/sqltest.php'>Home</a></h4>
+
 <?php
+
+#make variables for all of the links to sorting pages
+$book_id_a = "http://rrynda.wiktel.com/sqltest.php";
+$book_id_d = "http://rrynda.wiktel.com/sort/book_id_d.php";
+$title_a = "http://rrynda.wiktel.com/sort/title_a.php";
+$title_d = "http://rrynda.wiktel.com/sort/title_d.php";
+$year_published_a = "http://rrynda.wiktel.com/sort/year_published_a.php";
+$year_published_d = "http://rrynda.wiktel.com/sort/year_published_d.php";
+$shelf_id_a ="http://rrynda.wiktel.com/sort/shelf_id_a.php";
+$shelf_id_d ="http://rrynda.wiktel.com/sort/shelf_id_d.php";
+#make variables for the images displayed
+$asc = "https://imp.wiktel.com/media/img/admin/arrow-down.gif";
+$desc = "https://imp.wiktel.com/media/img/admin/arrow-up.gif";
+
 if($result = $mysqli->query($query))
 {
     #begin table, make the column headings
-    echo "<table><tr><th>Book ID</th><th>Titles</th><th>Year Published</th><th>Shelf ID</th></tr>\n";
-        # of the above line of code, the text-align function works.
+    #echo "<table><tr><th>Book ID</th><th>Titles</th><th>Year Published</th><th>Shelf ID</th></tr>\n";
+    #echo "<table><tr><th>Book ID<select name="sort"><option value='ascending'><option value='descending'></th> <th>Titles</th><th>Year Published</th><th>Shelf ID</th></tr>\n";
+    echo "<table><tr><th><a href='$book_id_d'>Book ID</a><img src='$asc'></th>
+    <th><a href='$title_a'>Titles</a><img src='$asc'></th>
+    <th><a href='$year_published_a'>Year Published</a><img src='$asc'></th>
+    <th><a href='$shelf_id_a'>Shelf ID</a><img src='$asc'></th></tr>\n";
+
 
         #while loop to run through and print all the data in the books table
         while($row = $result->fetch_assoc())
