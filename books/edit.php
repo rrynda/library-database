29fa4c2c26c31a78bd98014229359fe10aef00db
Ryan Rynda <rrynda@wiktel.com>
2016-06-17 09:07:01 -0500

    Add Books
    Ability to add books to the database added.

--- books/edit.php
+++ books/edit.php
@@ -43,52 +43,78 @@ if (array_key_exists("id", $_POST))
     $shelf_id = $_POST["shelf_id"];
       //print_r ($shelf_id);
 
-    $query2 = "UPDATE books SET title = '" . $mysqli->real_escape_string($title) . "',
-        year_published = '" . $mysqli->real_escape_string($year_published) . "',
-        shelf_id = '" . $mysqli->real_escape_string($shelf_id) . "'
-        WHERE id = '" . $mysqli->real_escape_string($id) . "'";
-      print_r ($query2);
-    $mysqli->query($query2);
+    print_r ($id);
+    if ($id != "") //if id != empty
+    {
+        $query2 = "UPDATE books SET title = '" . $mysqli->real_escape_string($title) . "',
+            year_published = '" . $mysqli->real_escape_string($year_published) . "',
+            shelf_id = '" . $mysqli->real_escape_string($shelf_id) . "'
+            WHERE id = '" . $mysqli->real_escape_string($id) . "'";
+        print_r ($query2);
+        $mysqli->query($query2);
+        
+        //CLEAN UP STUFF IN GENERAL
+        //if query succeeded
+            ?>
+            <p>
+                Your edit was made successfully.
+            </p>
+            <?php
+        //else
+    }
+    else //id id = empty
+    {
+        $query3 = "INSERT INTO books (title, year_published, shelf_id)" . 
+            "VALUES ( '" . $mysqli->real_escape_string($title) . "'," .
+            "'" . $mysqli->real_escape_string($year_published) . "', " .
+            "'" . $mysqli->real_escape_string($shelf_id) . "')";
+        print_r ($query3);
+        $mysqli->query($query3);
+        
+        //if query succeeded
+            ?>
+            <p>
+                Book was added successfully.
+            </p>
+            <?php
+        //else
+    }
 ?>
 
-<p>
-    Your edit was made successfully.
-</p>
 
 <?php
-}else
+}
+else
 {
-    if(!array_key_exists("id", $_REQUEST))
+    if (!array_key_exists("id", $_REQUEST))
     {
         echo "Book ID not entered.";
+        
+        //display empty form to add book
+        $id = null;
+        //$title = "Larry";
+        $title = null;
+        $year_published = null;
+        $shelf_id = null;
+        form($id, $title, $year_published, $shelf_id);
     }
     else
     {
         $id = $_REQUEST["id"];
         $query1 = "SELECT * FROM books WHERE id = '" . $mysqli->real_escape_string($id) . "'";
-        #print $query;
+        print_r ($query1);
 
         if ($result = $mysqli->query($query1))
         {
             if ($row = $result->fetch_assoc())
             {
-    ?>
-    <form method="post">
-    <div class="edit">
-    <p>
-    
-        Book ID: <?= $row["id"] ?><br>
-        Title: <input  name="title" type="text" value="<?php echo$row["title"] ?>"><br>
-        Year Published: <input name="year_published" type="text" value="<?= $row["year_published"] ?>"><br>
-        Shelf ID: <input name="shelf_id" type="text" value="<?= $row["shelf_id"] ?>"><br>
-        <a href=""><input type="submit" value="Save"></a>
-
-        <input type="hidden" name="id" value="<?= $id ?>">
-    </p>
-    </div>
-    </form>
-
-    <?php
+                $id = $row["id"];
+                $title = $row["title"];
+                $year_published = $row["year_published"];
+                $shelf_id = $row["shelf_id"];
+                
+                //display filled form to edit book
+                form($id, $title, $year_published, $shelf_id);
             }
             else
             {
@@ -98,6 +124,30 @@ if (array_key_exists("id", $_POST))
     }
 }
 
+
+function form($id, $title, $year_published, $shelf_id)
+{
+    ?>
+    <form method="post">
+    <div class="edit">
+    <p>
+    
+        Book ID: <?php echo $id ?><br>
+        Title: <input  name="title" type="text" value="<?= htmlspecialchars($title) ?>"><br>
+        Year Published: <input name="year_published" type="text" value="<?= htmlspecialchars($year_published) ?>"><br>
+        Shelf ID: <input name="shelf_id" type="text" value="<?= htmlspecialchars($shelf_id) ?>"><br>
+        
+            <a href=""><input type="submit" value="<?= $id ? 'Save':'Add'?>"></a>
+
+            <!-- <input type="hidden" name="id" value="<? // = $id ?>"> -->
+            <input type="hidden" name="id" value="<?= $id ?>">
+        
+    </p>
+    </div>
+    </form>
+    <?php
+    return "title, year_published, shelf_id"; //return data so query3 can do its thing
+}
 ?>
 </body>
 </html>
